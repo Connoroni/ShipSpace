27d27d8d35a31f2b6f05ff59351bb8fa334e05af
Connoroni <mitchell2k2@gmail.com>
2025-02-28 15:15:36 +0000

    background image of space, css variables for theme, module.css for article page

--- src/app/article/[title]/page.js
+++ src/app/article/[title]/page.js
@@ -1,6 +1,7 @@
 import { db } from "@/utils/dbConnectionString";
 import Image from "next/image";
 import InfoBox from "@/app/components/InfoBox";
+import styles from "./article.module.css";
 
 export default async function ArticlePage({ params }) {
   const titleParams = await params;
@@ -14,19 +15,19 @@ export default async function ArticlePage({ params }) {
   return (
     <>
       {pageData.map((article) => (
-        <section className="full-page" key={article.id}>
-          <div className="top-div">
-            <h1>{article.title}</h1>
+        <section className={styles.fullPage} key={article.id}>
+          <div className={styles.topDiv}>
+            <h1 className={styles.title}>{article.title}</h1>
             <p>{article.intro}</p>
           </div>
           {/* <InfoBox /> */}
-          <section className="main-content">
-            <div className="content-1">
+          <section className={styles.mainContent}>
+            <div className={styles.contentDiv}>
               {article?.sub1 ? (
-                <h2 className="subheading">{article?.sub1}</h2>
+                <h2 className={styles.subheading}>{article?.sub1}</h2>
               ) : null}
               {article?.content1 ? (
-                <p className="text-content">{article?.content1}</p>
+                <p className={styles.textContent}>{article?.content1}</p>
               ) : null}
               {article?.img1 ? (
                 <Image
@@ -37,12 +38,12 @@ export default async function ArticlePage({ params }) {
                 />
               ) : null}
             </div>
-            <div className="content-2">
+            <div className={styles.contentDiv}>
               {article?.sub2 ? (
-                <h2 className="subheading">{article?.sub2}</h2>
+                <h2 className={styles.subheading}>{article?.sub2}</h2>
               ) : null}
               {article?.content2 ? (
-                <p className="text-content">{article?.content2}</p>
+                <p className={styles.textContent}>{article?.content2}</p>
               ) : null}
               {article?.img2 ? (
                 <Image
@@ -53,12 +54,12 @@ export default async function ArticlePage({ params }) {
                 />
               ) : null}
             </div>
-            <div className="content-3">
+            <div className={styles.contentDiv}>
               {article?.sub3 ? (
-                <h2 className="subheading">{article?.sub3}</h2>
+                <h2 className={styles.subheading}>{article?.sub3}</h2>
               ) : null}
               {article?.content3 ? (
-                <p className="text-content">{article?.content3}</p>
+                <p className={styles.textContent}>{article?.content3}</p>
               ) : null}
               {article?.img3 ? (
                 <Image
