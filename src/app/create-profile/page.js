afa55a7d406411f5b82b64a46b4f8d88ec5475e1
Thomas Greening <tomfgreening@gmail.com>
2025-03-03 13:26:52 +0000

    Created form.

--- src/app/create-profile/page.js
+++ src/app/create-profile/page.js
@@ -26,7 +26,18 @@ export default async function CreateProfile() {
 
   return (
     <>
-      <h1>PLACEHOLDER</h1>
+      <h1>Create Profile</h1>
+      <form action={handleSubmit}>
+      <label htmlFor="username">Choose a username:</label>
+      <input type="text" name="username" id="username" />
+      <label htmlFor="email">Your e-mail address:</label>
+      <input type="text" name="email" id="email"/>
+      <label htmlFor="profile_pic">Upload a profile picture:</label>
+      <input type="text" name="profile_pic" id="profile_pic"/>
+      <label htmlFor="bio">Write a short bio:</label>
+      <input type="text" name="bio" id="bio"/>
+      </form>
+      <button type="submit">Submit</button>
     </>
   );
 }
