d094d96b286c275794abefd8ef2b04cce5257b60
Connoroni <mitchell2k2@gmail.com>
2025-03-04 16:48:28 +0000

    created dynamic tag page with list of articles, no styling yet

--- src/app/tags/[tag]/page.js
+++ src/app/tags/[tag]/page.js
@@ -3,12 +3,30 @@ import Link from "next/link";
 
 export default async function TagPage({ params }) {
   const tagParams = await params;
-  //     const tagData = (
-  //         db query here to select all articles where the tag matches the tag from the params, using the junction table
-  //     )
+  console.log("Tag params:", tagParams);
+  const tagData = (
+    await db.query(
+      `SELECT articles.title, tags.tag_name FROM articles
+        JOIN tag_junction ON articles.title = tag_junction.article_title
+        JOIN tags ON tag_junction.tag_id = tags.id
+        WHERE tags.id = $1`,
+      [tagParams.tag]
+    )
+  ).rows;
+  console.log(tagData);
   return (
     <>
-      <h1>PLACEHOLDER: Eventually there will be many articles linked here</h1>
+      <h1>{tagData[0].tag_name} Ships</h1>
+      <div>
+        <ul>
+          {tagData.map((article) => (
+            <li key={article.title}>
+              &bull;&nbsp;
+              <Link href={`/article/${article.title}`}>{article.title}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
     </>
   );
 }
