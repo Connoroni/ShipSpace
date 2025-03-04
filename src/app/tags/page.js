7d2e17683d6c6994e3241a7e996994baefe67bbb
Connoroni <mitchell2k2@gmail.com>
2025-03-04 11:14:37 +0000

    tags page now has a map through the tags table

--- src/app/tags/page.js
+++ src/app/tags/page.js
@@ -1,4 +1,21 @@
+import { db } from "@/utils/dbConnectionString";
+import Link from "next/link";
 
-export default function Tags() {
-    return <h1>Tags</h1>
+export default async function Tags() {
+  const tagList = (await db.query(`SELECT * FROM tags`)).rows;
+
+  return (
+    <>
+      <h1>Tags</h1>
+      <div>
+        <ul>
+          {tagList.map((tag) => (
+            <li key={tag.id}>
+              &bull;&nbsp;<Link href="#">{tag.tag_name}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
 }
