c11bd37daf1059a232ab3a78289518c68f304409
Connoroni <mitchell2k2@gmail.com>
2025-03-06 10:35:30 +0000

    added dropdowns to select tags, different queries depending on if tag2 is blank

--- src/app/new-article/page.js
+++ src/app/new-article/page.js
@@ -2,7 +2,9 @@ import { db } from "@/utils/dbConnectionString";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
-export default function NewArticlePage() {
+export default async function NewArticlePage() {
+  const tagList = (await db.query(`SELECT * FROM tags ORDER BY tag_name`)).rows;
+
   async function handleSubmit(formValues) {
     "use server";
     const formData = {
@@ -19,7 +21,9 @@ export default function NewArticlePage() {
       sub3: formValues.get("sub3"),
       content3: formValues.get("content3"),
       img3: formValues.get("img3"),
-      alt3: formValues.et("alt3"),
+      alt3: formValues.get("alt3"),
+      tag1: formValues.get("tag1"),
+      tag2: formValues.get("tag2"),
     };
 
     db.query(
@@ -41,6 +45,21 @@ export default function NewArticlePage() {
         formData.alt3,
       ]
     );
+
+    {
+      tag2 = "none"
+        ? db.query(
+            `INSERT INTO tag_junction (article_title, tag_id) 
+      VALUES ($1, $2)`,
+            [formData.title, formData.tag1]
+          )
+        : db.query(
+            `INSERT INTO tag_junction (article_title, tag_id) 
+      VALUES ($1, $2), ($1, $3)`,
+            [formData.title, formData.tag1, formData.tag2]
+          );
+    }
+
     redirect("/");
     revalidatePath("/");
   }
@@ -123,6 +142,24 @@ export default function NewArticlePage() {
 
         <label htmlFor="">image discription: </label>
         <input type="text" name="alt3" id="alt3" className="text-black" />
+
+        <label htmlFor="tag1">Select a tag</label>
+        <select id="tag1" name="tag1">
+          {tagList.map((tag) => (
+            <option key={tag.id} value={tag.tag_name}>
+              {tag.tag_name}
+            </option>
+          ))}
+        </select>
+
+        <select id="tag2" name="tag2">
+          <option value="none">N/A</option>
+          {tagList.map((tag) => (
+            <option key={tag.id} value={tag.tag_name}>
+              {tag.tag_name}
+            </option>
+          ))}
+        </select>
 
         <button
           type="submit"
