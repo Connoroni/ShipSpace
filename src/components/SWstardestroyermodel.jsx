2c111d6b8701aa5903a9f7c0e7ee09c6ae1ee00c
Thomas Greening <tomfgreening@gmail.com>
2025-03-06 14:07:59 +0000

    Defined loadin agnle for 3d model.

--- src/components/SWstardestroyermodel.jsx
+++ src/components/SWstardestroyermodel.jsx
@@ -13,9 +13,9 @@ import { useGLTF } from "@react-three/drei";
 export default function Model(props) {
   const { nodes, materials } = useGLTF("/LOWRESimperial_i-class_star_destroyer.glb");
   return (
-    <group {...props} dispose={null} scale={[5.1, 5.1, 5.1]}>
-      <group rotation={[-Math.PI / 4, 6, 1]}>
-        <group rotation={[Math.PI / 2, 6, 6]}>
+    <group {...props} dispose={null} scale={[3.5, 3.5, 3.5]}>
+      <group rotation={[-Math.PI / 1, 3, 1]}>
+        <group rotation={[Math.PI / 2, 1, 1]}>
           <mesh
             geometry={nodes.defaultMaterial.geometry}
             material={materials.StarDestroyer_BasicHull}
